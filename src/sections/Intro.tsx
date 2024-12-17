d34df40fc07cbb8e95880b4e9cf8cbc32cb24d30
Dinbandhu12 <dinbandhu8445@gmail.com>
2024-12-17 22:11:57 +0530

    added source code

---
+++ src/sections/Intro.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+const Intro: FC = () => {
+  return (
+    <section className="section mt-12 md:mt-16 lg:mt-20">
+      <div className="px-4 md:px-8 lg:px-16">
+        <h2 className="text-6xl md:text-7xl lg:text-8xl lg:w-[80%]">Building beautiful websites with clean code and thoughtful design to help your business grow and stand out online.
+        </h2>
+
+      </div>
+      
+    </section>
+  );
+};
+
+export default Intro;
